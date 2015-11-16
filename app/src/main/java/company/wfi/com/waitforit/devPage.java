f37c9a972452f158893b7cb2418d0fd30bb0b298
yarivg <yarivgavriel2@gmail.com>
2015-11-16 12:23:19 +0200

    Register through Google. הרשמה מול גוגל.(המידע נשמר באפליקציה נכון לעכשיו, ולא במסד נתונים)*כרגע אין אופצית התנתקות. Fixed YoutubePlayer on FullScreen. תיקון סרטון יוטיוב פועל בצורה אנכית ואופקית על כל המכשירים. The app save a session for the user. במידה ומשתמש התחבר דרך גוגל הוא יועבר ישירות לדף בחירת קטגוריות. The playlist knows when the timer is over. התייחסות לטיימרים וסיום המתנה בערך כ20 שניות לסיום אם נגמר תוכן כלשהו.
    התחלת המתנה מועברת אל דף בחירת זמן.
    New Waiting Time PIctures.
    עודכנו התמונות של עומר לזמני ההמתנה החדשים.
    Click on Waiting Time Pictures.
    בעת לחיצה על תמונת ההמתנה היא נצבעת בכתום ומתחילה ההמתנה.

--- app/src/main/java/company/wfi/com/waitforit/devPage.java
+++ app/src/main/java/company/wfi/com/waitforit/devPage.java
@@ -2,17 +2,14 @@ package company.wfi.com.waitforit;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-/**
- * Created by User on 17/10/2015.
- */
 public class devPage extends Activity implements View.OnClickListener {
 
     ImageButton back;
-    //TODO check if user is logged in - and display setting icon instead of login icon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +18,37 @@ public class devPage extends Activity implements View.OnClickListener {
         back = (ImageButton)findViewById(R.id.backbtn);
         back.setOnClickListener(this);
 
-    }
 
+        Class<?> activityClass;
+        try {
+            SharedPreferences prefs = getSharedPreferences("X", MODE_PRIVATE);
+            activityClass = Class.forName(
+                    prefs.getString("lastActivityforDev", settingcl.class.getName()));
+            if(String.valueOf(activityClass).contains("firstAct")){
+                back.setTag("firstAct");
+                back.setImageResource(R.drawable.backtologin);
+            //if the user came from login page than login image shold appear. Otherwise setting image
+            }
+            else {
+                back.setImageResource(R.drawable.settings);
+                back.setTag("settings");
+            }
+                SharedPreferences.Editor e = prefs.edit();
+            e.remove("lastActivityforDev");
+            e.commit();
+        } catch(ClassNotFoundException ex) {
+            back.setImageResource(R.drawable.settings);
+            back.setTag("settings");
+        }
+    }
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.backbtn:
-                startActivity(new Intent(this,firstAct.class));
+                if(back.getTag().equals("firstAct"))
+                    startActivity(new Intent(this,firstAct.class));
+                else
+                    startActivity(new Intent(this,settingcl.class));
                 break;
         }
     }
--- app/src/main/java/company/wfi/com/waitforit/firstAct.java
+++ app/src/main/java/company/wfi/com/waitforit/firstAct.java
@@ -1,13 +1,21 @@
 package company.wfi.com.waitforit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -16,19 +24,40 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
 
-/**
- * Created by User on 17/10/2015.
- */
-public class firstAct extends Activity implements View.OnClickListener {
+public class firstAct extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener {
 
-    ImageButton wfi,google,facebook,set;
+    ImageButton google;
+    ImageButton facebook;
+    ImageButton set;
+    ImageButton wfi;
+    int session;
+    SharedPreferences.Editor editor;
+    SharedPreferences prefs;
+
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "My Login App";
+    private GoogleApiClient googleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,25 +71,62 @@ public class firstAct extends Activity implements View.OnClickListener {
         set = (ImageButton)findViewById(R.id.setbtn);
         set.setOnClickListener(this);
         wfi.setOnClickListener(this);
-    }
+        prefs = getSharedPreferences("X", MODE_PRIVATE);
+        //Session
+        session = prefs.getInt("Session",0);
+        if(session != 0){
+            //When the user is logged in
+            Toast.makeText(this, String.valueOf("Connected Successfully through Google!"),Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this,categorypage.class));
+            finish();
+        }
 
+        //TODO logout app makes session zero
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().requestScopes(new Scope(Scopes.PLUS_LOGIN)).requestIdToken(getString(R.string.server_client_id))
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(gso.getScopeArray());
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+    public boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.wfibutton:
+                prefs = getSharedPreferences("X", MODE_PRIVATE);
+                editor = prefs.edit();
+                editor.putString("lastActivityforDev", String.valueOf(firstAct.class.getName()));
+                editor.commit();
                 startActivity(new Intent(this,devPage.class));
                 break;
             case R.id.googlebutton:
-                startActivity(new Intent(this, categorypage.class));
-                //TODO login with google and update database
+
                 break;
             case R.id.facebookbutton:
                 Toast.makeText(this,"Logging through Facebook",Toast.LENGTH_SHORT).show();
-                //TODO login with facebook and update database
+                startActivity(new Intent(this, categorypage.class));
+                //TODO(later) login with facebook and update database
                 break;
             case R.id.setbtn:
-                SharedPreferences prefs = getSharedPreferences("X", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
+                prefs = getSharedPreferences("X", MODE_PRIVATE);
+                editor = prefs.edit();
                 editor.putString("lastActivity", String.valueOf(firstAct.class.getName()));
                 editor.commit();
                 startActivity(new Intent(this, settingcl.class));
@@ -76,13 +142,32 @@ public class firstAct extends Activity implements View.OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
+        if(!isNetworkAvailable(this)){
+            ShowDialog();
+        }
     }
-
+    @Override
+    public void onBackPressed() {
+    }
+    private void ShowDialog(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("No Internet Connection");
+// Add the buttons
+        builder.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                if(!isNetworkAvailable(getApplicationContext())){
+                     builder.show();
+                }
+            }
+        });
+// Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
     @Override
     protected void onStart() {
         super.onStart();
     }
-
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -97,4 +182,59 @@ public class firstAct extends Activity implements View.OnClickListener {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            //acct.geteverything
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            String personToken = acct.getIdToken();
+            prefs = getSharedPreferences("X", MODE_PRIVATE);
+            editor = prefs.edit();
+            editor.putString("UserName",personName);
+            editor.putString("UserEmail",personEmail);
+            editor.putString("UserId",personId);
+            editor.putString("UserPhoto", String.valueOf(personPhoto));
+            editor.putString("UserToken", personToken);
+            editor.putInt("Session",1);
+            editor.commit();
+            //String x1 = acct.zzmw();
+            //Set<Scope> myScopes = acct.getGrantedScopes();//2,4
+            Toast.makeText(this, String.valueOf("Connected Successfully!"),Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, categorypage.class));
+
+            //updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+            Toast.makeText(this,"ID Token: null",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
 }
